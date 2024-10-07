3f3dd6b038196867ac6356f05e55e6e34f7e6a9e
Michal Manda <mandamic@fit.cvut.cz>
2024-10-07 17:49:28 +0200

    Add validation support to StringListInput

--- src/lib/components/StringListInput.svelte
+++ src/lib/components/StringListInput.svelte
@@ -2,53 +2,51 @@
     import { faRemove } from "@fortawesome/free-solid-svg-icons";
     import Fa from "svelte-fa";
 
-    /**
-     * @type {string[]}
-     */
-     export let values = []
+    /** @type {string[]} */
+    export let values = [];
 
-     /**
-     * @type {string}
-     */
-     export let name = ""
+    /** @type {string} */
+    export let name = "";
 
-     /**
-     * @type {string}
-     */
-     let newValue = ""
+    /** @type {boolean} */
+    export let required = false;
 
-     /**
-     * @param {number} index
-     */
-     function removeValue(index) {
-        values.splice(index, 1)
-        values = values
-     }
+    /** @type {string} */
+    let newValue = "";
 
-     function addValue() {
-        values = [ ...values, newValue ]
-     }
+    /** @type {boolean} */
+    $: showAddRequired = required && values.length == 0;
 
-     $: {
+    /** @param {number} index*/
+    function removeValue(index) {
+        values.splice(index, 1);
+        values = values;
+    }
+
+    function addValue() {
+        values = [...values, newValue];
+    }
+
+    $: {
         if (newValue != "") {
-            addValue()
-            newValue = ""
+            addValue();
+            newValue = "";
         }
-     }
+    }
 </script>
 
 {#each values as value, index}
     <div class="flex">
         <div class="input-group input-group-divider">
             <!-- svelte-ignore a11y-autofocus -->
-            <input name="{name}.{index}.value" type="text" bind:value={value} autofocus  />
+            <input name="{name}.{index}.value" type="text" placeholder={required ? "Povinné" : ""} bind:value autofocus />
         </div>
         <button class="btn-icon variant-filled-error ml-2" on:click={() => removeValue(index)}>
-            <Fa icon={faRemove}/>
+            <Fa icon={faRemove} />
         </button>
     </div>
 {/each}
 
 <div class="input-group input-group-divider">
-    <input type="text" placeholder="Přidat další..." bind:value={newValue} />
+    <input type="text" placeholder="{showAddRequired ? "Povinné" : "Přidat další..."}" bind:value={newValue} />
 </div>
