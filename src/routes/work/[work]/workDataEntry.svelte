21eb04813ffa0562c40b7b5838d83017bfb3e4dd
Michal Manda <mandamic@fit.cvut.cz>
2024-10-08 12:10:24 +0200

    Validation POC

--- src/routes/work/[work]/workDataEntry.svelte
+++ src/routes/work/[work]/workDataEntry.svelte
@@ -1,14 +1,30 @@
 <script>
     import StringListInput from "$lib/components/StringListInput.svelte";
+    import { createForm } from "felte";
+    import { validator } from "@felte/validator-yup";
+    import * as yup from 'yup';
+
+    const schema = yup.object({
+        title: yup.string().required(),
+        genre: yup.array().of(yup.object({
+            value: yup.string().required()
+        })).min(1).required(),
+    });
+
+    const { form, errors, isValid } = createForm({
+        extend: validator({ schema }),
+    });
 
     /** @type {import("$lib/schemas/work").Work} */
     export let data;
 </script>
 
-<form class="form border border-surface-500 m-2 p-4 space-y-2 rounded-container-token">
+<form use:form class="form border border-surface-500 m-2 p-4 space-y-2 rounded-container-token">
+    <span>{JSON.stringify($errors)}</span>
+    <span>{$isValid}</span>
     <p>Název díla</p>
     <div class="input-group input-group-divider">
-        <input type="text" bind:value={data.title} />
+        <input type="text" name="title" placeholder="Povinné" bind:value={data.title} />
     </div>
     <p>Alternativní název</p>
     <div class="input-group input-group-divider">
@@ -27,7 +43,7 @@
         <input type="text" bind:value={data.yearOfPublication} />
     </div>
     <p>Žánry</p>
-    <StringListInput bind:values={data.genre} />
+    <StringListInput required={true} name="genre" bind:values={data.genre} />
     <p>Klasifikace - Doba</p>
     <StringListInput bind:values={data.classificationTime} />
     <p>Klasifikace - Lokace</p>
