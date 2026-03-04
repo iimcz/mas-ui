947fe5c52df325a29bee19c93371a528684022af
Michal Manda <mandamic@fit.cvut.cz>
2026-03-04 11:51:43 +0100

    Format game package data entry

--- src/lib/components/process/GamePackageDataEntry.svelte
+++ src/lib/components/process/GamePackageDataEntry.svelte
@@ -1,17 +1,27 @@
-<script>
-    
-    /**
-     * @typedef {Object} Props
-     * @property {import("$lib/schemas/gamePackage").GamePackage} data
-     */
-
-    /** @type {Props} */
-    let { data = $bindable() } = $props();
+<script lang="ts">
+    import type { GamePackage } from "$lib/schemas/gamePackage";
+    import Blockquote from "../Blockquote.svelte";
+    let { data = $bindable() }: { data: GamePackage } = $props();
 </script>
 
-<form class="form border border-surface-500 m-2 p-4 space-y-2 rounded-container-token">
-    <p>Název</p>
-        <div class="input-group input-group-divider">
-        <input type="text" bind:value={data.name} />
-    </div>
+<form class="form border border-surface-500 m-2 p-4 space-y-2 rounded-container">
+    <div>Název</div>
+    <input class="input" type="text" bind:value={data.name} />
+
+    {#if data.conversionDate}
+        <div>Datum konverze</div>
+        <Blockquote>{data.conversionDate}</Blockquote>
+    {/if}
 </form>
+
+<style scoped>
+    @reference "#layout.css";
+
+    .form > *:nth-child(2n + 1) {
+        @apply mt-4 text-lg;
+    }
+
+    .form > *:nth-child(1) {
+        @apply mt-0;
+    }
+</style>
